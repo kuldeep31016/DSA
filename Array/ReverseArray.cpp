246cc34e08597462fdf292716437562157c6b459
kuldeepraj <kr9667102@gmail.com>
2026-03-18 21:50:51 +0530

    Add ReverseArray.cpp to implement array reversal functionality

---
+++ Array/ReverseArray.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size){
+   
+    int index = 0;
+    int high = size - 1;
+
+    while(index < high){
+        // swap
+        int temp = arr[index];
+        arr[index] = arr[high];
+        arr[high] = temp;
+
+        index++;
+        high--;
+    }
+
+    cout << "Reversed Array: ";
+
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+
+    int arr[5] = {7,6,5,4,3};
+
+    reverseArray(arr, 5);
+
+    return 0;
+}
